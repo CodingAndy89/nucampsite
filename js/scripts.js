d17427654d31758db7dc1615f9a65791d861af51
Andrew Kennedy <kennedyandrew33@gmail.com>
2024-02-18 11:01:40 -0800

    Week 5 Workshop Weather App Added

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,41 @@ carouselButton.addEventListener('click', function () {
         carousel.cycle();
     }
 })
+
+async function fetchWeather() {
+    try {
+        const apiKey = process.env.OPEN_WEATHER_API_KEY
+        const city = 'Lacey';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data);
+        displayWeather(data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function displayWeather(data) {
+    const temp = data.main.temp;
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+
+    const img = document.createElement('img');
+    img.src = `https://openweathermap.org/img/w/${icon}.png`;
+
+    const container = document.getElementById('weather');
+    container.appendChild(img);
+
+    const tempElement = document.createElement('p');
+    tempElement.textContent = `${temp}\u00B0 F`;
+    container.appendChild(tempElement);
+
+    const descElement = document.createElement('p');
+    descElement.textContent = description;
+    container.appendChild(descElement);
+}
+
+fetchWeather();
